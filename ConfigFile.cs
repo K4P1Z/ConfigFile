22ec7b1f28843ed6e433d1da614170f26ed0df22
Kacper <144652457+K4P1Z@users.noreply.github.com>
2024-10-10 14:15:50 +0200

    Add files via upload

---
+++ ConfigFile.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp1
+{  
+        class ConfigFile
+        {
+            string Path;
+            string EXE = Assembly.GetExecutingAssembly().GetName().Name;
+            [DllImport("kernel32", CharSet = CharSet.Unicode)]
+            static extern long WritePrivateProfileString(string Section, string Key, string value, string Filepath);
+
+            [DllImport("kernel32", CharSet = CharSet.Unicode)]
+            static extern long GetPrivateProfileString(string Section, string Key, string Default, StringBuilder ReVal, int Size, string Filepath);
+
+            public ConfigFile(string IniPath = null)
+            {
+                Path = new FileInfo(IniPath ?? EXE + ".ini").FullName.ToString();
+            }
+            public string Read(string Key, string Section = null)
+            {
+                var RetVal = new StringBuilder(255);
+                GetPrivateProfileString(Section ?? EXE, Key, "", RetVal, 255, Path);
+                return RetVal.ToString();
+            }
+            public void Write(string Key, string Value, string Section = null)
+            {
+                WritePrivateProfileString(Section ?? EXE, Key, Value, Path);
+            }
+        public void DeleteKey(string Key, string Section= null)
+        {
+            Write(Key, null, Section ?? EXE);
+
+        }
+        public void DeleteSection(string Section = null)
+        {
+            Write(null,null, Section ?? EXE);
+        }
+        }
+       
+}
+
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lekcja programowania");
+
+            string wartosc_parametru;
+
+            var Ini = new ConfigFile(@".\config.ini");
+
+            wartosc_parametru = Ini.Read("ipaddress", "Drukarka");
+            Console.WriteLine("IP address=" + wartosc_parametru);
+
+            wartosc_parametru = Ini.Read("port", "Drukarka");
+            Console.WriteLine("Port=" + wartosc_parametru);
+
+            string nazwa_drukarki;
+            wartosc_parametru = Ini.Read("nazwadrukarki", "Drukarka");
+
+            Console.ReadLine();
+        }
+    }
+
+   
+}
